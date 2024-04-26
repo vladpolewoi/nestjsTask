fe587d16be232cd0f51ea3e203e6d18484c5d4bc
vlad polewoi <vlad.polewoi1@gmail.com>
2024-04-26 12:54:00 +0300

    feat: 404 to Not existings Task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid'
 
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { Task, TaskStatus } from './task.model'
 import { CreateTaskDto } from './dto/create-task.dto'
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
@@ -32,7 +32,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id)
+    const found = this.tasks.find((task) => task.id === id)
+
+    if (!found) {
+      throw new NotFoundException('Task not found')
+    }
+
+    return found
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
