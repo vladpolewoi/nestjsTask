1e538b359e52d36b32c91e4b6f9ea294306b8d86
vlad polewoi <vlad.polewoi1@gmail.com>
2024-05-11 20:01:29 +0300

    feat: Sign Up Functionality

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,6 +1,9 @@
 import { Repository } from 'typeorm'
 import { User } from './user.entity'
 import { InjectRepository } from '@nestjs/typeorm'
+import { AuthCredentialsDto } from './dto/auth-credentials.dto'
+import { ConflictException, InternalServerErrorException } from '@nestjs/common'
+import * as bycrypt from 'bcrypt'
 
 export class UsersRepository extends Repository<User> {
   constructor(
@@ -9,5 +12,25 @@ export class UsersRepository extends Repository<User> {
   ) {
     const { target, manager, queryRunner } = usersRepository
     super(target, manager, queryRunner)
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto
+
+    //hash
+    const salt = await bycrypt.genSalt()
+    const hashedPassword = await bycrypt.hash(password, salt)
+
+    const user = this.create({ username, password: hashedPassword })
+
+    try {
+      await this.save(user)
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists')
+      } else {
+        throw new InternalServerErrorException()
+      }
+    }
   }
 }
