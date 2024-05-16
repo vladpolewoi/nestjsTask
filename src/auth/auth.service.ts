f14e829b798312f7a11b78dfd3fd2b98bdf001f5
vlad polewoi <vlad.polewoi1@gmail.com>
2024-05-16 18:41:17 +0300

    feat: Auth part 1

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,21 +2,31 @@ import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { UsersRepository } from './users.repository'
 import { AuthCredentialsDto } from './dto/auth-credentials.dto'
 import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt'
+import { JwtPayload } from './jwt-payload.interface'
 
 @Injectable()
 export class AuthService {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(
+    private usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto)
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto
     const user = await this.usersRepository.findOneBy({ username })
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'Success'
+      const payload: JwtPayload = { username }
+      const accessToken: string = this.jwtService.sign(payload)
+
+      return { accessToken }
     } else {
       throw new UnauthorizedException('Please check your login credentials')
     }
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,10 +5,11 @@ import { TasksController } from './tasks.controller'
 import { TasksService } from './tasks.service'
 import { TasksRepository } from './tasks.repository'
 import { Task } from './tasks.entity'
+import { AuthModule } from 'src/auth/auth.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService, TasksRepository],
 })
-export class TasksModule {}
+export class TasksModule { }
